f8038be9a111bb072876381e6038473664191616
Yemelia <yemelianovnk@gmail.com>
2018-08-06 22:31:45 +0300

    add snippet details and actions

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const moment = require('moment');
+const { highlight } = require('highlight.js');
 
-const snippets = require('./snippets.json');
+let snippets = require('./snippets.json');
 
 const server = express();
 
 server.set('view engine', 'pug');
 
-server.locals.moment = moment;
+server.locals.moment = moment; // Set global variable
 
 server.use(express.static('public'));
 server.use('/lib', express.static('node_modules'));
@@ -23,6 +24,34 @@ server.get('/create', (req, res) => {
     res.render('form', {
         snippet: {}
     });
+});
+
+server.get('/snippet/:snippetId', (req, res) => {
+    const snippet = snippets.find(s => s.id == req.params.snippetId);
+    if (!snippet) return res.redirect('/');
+
+    res.render('view', {
+        snippet,
+        highlight,
+    });
+});
+
+server.get('/snippet/:snippetId/edit', (req, res) => {
+    const snippet = snippets.find(s => s.id == req.params.snippetId);
+    if (!snippet) return res.redirect('/');
+
+    res.render('form', {
+        snippet,
+    });
+});
+
+server.post('/snippet/:snippetId/delete', (req, res) => {
+    const snippet = snippets.find(s => s.id == req.params.snippetId);
+    if (!snippet) return res.redirect('/');
+
+    snippets = snippets.filter(s => s.id != req.params.snippetId);
+
+    return res.redirect('/');
 });
 
 server.listen(3000, () => console.log('Server run on http://localhost:3000'));
