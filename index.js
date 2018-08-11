b4881974ce104df82a042c0580e0033ca41db653
Yemelia <yemelianovnk@gmail.com>
2018-08-11 16:15:53 +0300

    add editor + refactoring

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const moment = require('moment');
+const crypto = require('crypto');
 const { highlight } = require('highlight.js');
 
 let snippets = require('./snippets.json');
@@ -12,6 +13,7 @@ server.locals.moment = moment; // Set global variable
 
 server.use(express.static('public'));
 server.use('/lib', express.static('node_modules'));
+server.use(express.urlencoded({ extended: false }));
 
 server.get('/', (req, res) => {
     res.render('index', {
@@ -20,13 +22,31 @@ server.get('/', (req, res) => {
     });
 });
 
-server.get('/create', (req, res) => {
+server.get('/snippets/create', (req, res) => {
     res.render('form', {
         snippet: {}
     });
 });
 
-server.get('/snippet/:snippetId', (req, res) => {
+server.get('/snippets/search', (req, res) => {
+    if (!req.query.query) return res.redirect('/');
+
+    const query = req.query.query.toLowerCase();
+
+    const foundSnippets = snippets.filter(s =>
+        s.filename.toLowerCase().includes(query) ||
+        s.description.toLowerCase().includes(query) ||
+        s.language.toLowerCase().includes(query)
+    );
+
+    res.render('search', {
+        title: 'Результаты поиска',
+        query: req.query.query,
+        snippets: foundSnippets
+    });
+});
+
+server.get('/snippets/:snippetId', (req, res) => {
     const snippet = snippets.find(s => s.id == req.params.snippetId);
     if (!snippet) return res.redirect('/');
 
@@ -36,7 +56,7 @@ server.get('/snippet/:snippetId', (req, res) => {
     });
 });
 
-server.get('/snippet/:snippetId/edit', (req, res) => {
+server.get('/snippets/:snippetId/edit', (req, res) => {
     const snippet = snippets.find(s => s.id == req.params.snippetId);
     if (!snippet) return res.redirect('/');
 
@@ -45,7 +65,36 @@ server.get('/snippet/:snippetId/edit', (req, res) => {
     });
 });
 
-server.post('/snippet/:snippetId/delete', (req, res) => {
+server.post('/snippets/create', (req, res) => {
+    const snippet = {
+        id: crypto.randomBytes(8).toString('hex'),
+        createdAt: Date.now(),
+        ...req.body
+    };
+    
+    snippets.push(snippet);
+
+    res.redirect('/');
+});
+
+server.post('/snippets/:snippetId/edit', (req, res) => {
+    const snippet = snippets.find(s => s.id == req.params.snippetId);
+
+    if (!snippet) return res.redirect('/');
+
+    snippets = snippets.map(s => {
+        if (s.id != snippet.id) return s;
+        else return {
+            ...s,
+            ...req.body,
+            updatedAt: Date.now(),
+        };
+    });
+
+    res.redirect(`/snippets/${snippet.id}`);
+});
+
+server.post('/snippets/:snippetId/delete', (req, res) => {
     const snippet = snippets.find(s => s.id == req.params.snippetId);
     if (!snippet) return res.redirect('/');
 
